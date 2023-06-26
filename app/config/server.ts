2da09830941a1be28d096560f1b6da8bd9f5b3be
ledudude <117006520+ledudude@users.noreply.github.com>
2023-06-26 15:57:48 +0800

    Update server.ts

--- app/config/server.ts
+++ app/config/server.ts
@@ -41,7 +41,7 @@ export const getServerSideConfig = () => {
   const apiKey = apiKeys.at(Math.floor(Math.random()* apiKeys.length)) ?? ''
 
   return {
-    apiKey: process.env.OPENAI_API_KEY,
+    apiKey,
     code: process.env.CODE,
     codes: ACCESS_CODES,
     needCode: ACCESS_CODES.size > 0,
